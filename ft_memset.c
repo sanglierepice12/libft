03db6619af4c646da63c23e2e455df2e5ddeb648
gsuter <gonzague1412@gmail.com>
2023-11-13 15:58:27 +0100

    add ex

--- ft_memset.c
+++ ft_memset.c
@@ -1,33 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset.c                                           :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gostr <gostr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:43:41 by gostr             #+#    #+#             */
-/*   Updated: 2023/11/08 12:43:42 by gostr            ###   ########.fr       */
+/*   Updated: 2023/11/13 15:56:54 by gostr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-void memset(void *ptr, int c, size_t len)
+void *ft_memset(void *pointer, int value, size_t count)
 {
-
-}
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include"libft.h"
-#include<string.h>
-
-int main(int arc, char **argv)
-{
-    if (arc < 1)
-        return (0);
-    printf("is nb ? : %d\n", memset(void *b, int c, size_t len));
-    printf("is nb ? : %lu\n", strlen(argv[1]));
-    return (0);
+    int i;
+    
+    i = 0;
+    while(i < count)
+    {
+        ((char *) pointer)[i] = value;
+        i++;
+    }
 }
